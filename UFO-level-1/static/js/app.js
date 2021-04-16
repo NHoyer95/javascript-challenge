11c2aefce75905ea7123e10bbe561e4f09db5b20
NHoyer95 <hoyer047@umn.edu>
2021-04-16 14:38:18 -0500

    Updates to app.py

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create table to hold data
+var tbody = d3.select("tbody");
+
+// for each array, insert into the table body
+tableData.forEach(ufoSighting => {
+
+    // add a new row for each entry
+    var newRow = tbody.append("tr");
+
+    // Loop through UFO sightings
+    Object.defineProperties(ufoSighting).forEach(function([key, value]) {
+
+        // check console to make sure entries are being logged
+        console.log(key, value);
+
+        // add to the table in a new cell
+        var newCell = newRow.append("td");
+        newCell.text(value);
+    })
+})
